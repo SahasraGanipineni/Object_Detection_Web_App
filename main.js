43e549e84693f3f94bbac1bab5ecc5830535972a
SahasraGanipineni <87746901+SahasraGanipineni@users.noreply.github.com>
2021-12-22 15:38:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function book_screen() {
+    window.location = "book.html";
+}
+
+function back() {
+    window.location = "index.html";
+}
+
+function bottles_screen() {
+    window.location = "bottle.html";
+}
+
+function fruits_screen() {
+    window.location = "fruits.html";
+}
+
+function home_interior_screen() {
+    window.location = "hall.html";
+}
+
+function trex_screen() {
+    window.location = "trex.html";
+}
